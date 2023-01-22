a3a1ceb6f77959da04078e76831ebb8ae4ddb917
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2023-01-22 17:32:31 +0900

    decrease input bit

--- clock_divide_select_4ch.srcs/sim_1/imports/src/top_tb.v
+++ clock_divide_select_4ch.srcs/sim_1/imports/src/top_tb.v
@@ -1,30 +1,27 @@
-`timescale 10ns/1ns
+`timescale 100ns/1ns
 
 module top_tb;
     //define signal
-    reg     r_clk;
-    reg     [34:0]   r_io_in;
+    reg     [26:0]   r_io_in;
     wire    w_out;
     //reg     [4:0]   r_clock_counter_a;
     //reg     [4:0]   r_clock_div_factor_a;
 
     //1MHz clock signal generation
     initial begin
-        r_clk = 0;
-        r_io_in = 35'b1_00000011_00000010_00000001_00000000_00;
+        r_io_in = 26'b000011_000010_000111_000000_011;
     end
             
     always #(1)
-        r_clk <= ~r_clk;
-    always #(400)
         r_io_in[0] <= ~r_io_in[0];
-    always #(200)
+    always #(400)
         r_io_in[1] <= ~r_io_in[1];
+    always #(200)
+        r_io_in[2] <= ~r_io_in[2];
     //read out top module
     top top_inst(
         //.clock_div_factor_a (r_clock_div_factor_a),
         //.clock_counter_a    (r_clock_counter_a),
-        .clk        (r_clk),
         .io_in      (r_io_in),
         .out        (w_out)
     );
--- clock_divide_select_4ch.srcs/sources_1/imports/src/top.v
+++ clock_divide_select_4ch.srcs/sources_1/imports/src/top.v
@@ -1,32 +1,31 @@
 `timescale 10ns/1ns
 
 module top(
-        input   wire    clk,
-        input   wire    [34:0] io_in,
+        input   wire    [26:0] io_in,
         output  wire    out
     );
     
-    reg [8:0]   clock_counter_a     =   9'b000000000;
-    reg [8:0]   clock_counter_b     =   9'b000000000;
-    reg [8:0]   clock_counter_c     =   9'b000000000;
-    reg [8:0]   clock_counter_d     =   9'b000000000;
+    reg [6:0]   clock_counter_a     =   7'b0000000;
+    reg [6:0]   clock_counter_b     =   7'b0000000;
+    reg [6:0]   clock_counter_c     =   7'b0000000;
+    reg [6:0]   clock_counter_d     =   7'b0000000;
     reg [3:0]   div_clock         =   4'b0000;
 
-    wire [7:0]  clock_div_factor_a;
-    wire [7:0]  clock_div_factor_b;
-    wire [7:0]  clock_div_factor_c;
-    wire [7:0]  clock_div_factor_d;
+    wire [5:0]  clock_div_factor_a;
+    wire [5:0]  clock_div_factor_b;
+    wire [5:0]  clock_div_factor_c;
+    wire [5:0]  clock_div_factor_d;
     wire [1:0]  clock_select;
-    wire        enable;
+    wire        clk;
     wire        clock_syn;
     
-    assign  clock_select = io_in[1:0];
-    assign  enable = io_in[34];
-    assign  clock_syn = (enable)? div_clock[clock_select]:0;
-    assign  clock_div_factor_a = io_in[9:2];
-    assign  clock_div_factor_b = io_in[17:10];
-    assign  clock_div_factor_c = io_in[25:18];
-    assign  clock_div_factor_d = io_in[33:26];
+    assign  clk = io_in[0];
+    assign  clock_select = io_in[2:1];
+    assign  clock_syn = div_clock[clock_select];
+    assign  clock_div_factor_a = io_in[8:3];
+    assign  clock_div_factor_b = io_in[14:9];
+    assign  clock_div_factor_c = io_in[20:15];
+    assign  clock_div_factor_d = io_in[26:21];
     
     
     always @ (posedge clk) begin
@@ -37,19 +36,19 @@ module top(
 
         if (clock_div_factor_a < clock_counter_a) begin
             div_clock[0] <= ~div_clock[0];
-            clock_counter_a <= 9'b000000000;
+            clock_counter_a <= 7'b0000000;
         end
         if (clock_div_factor_b < clock_counter_b) begin
             div_clock[1] <= ~div_clock[1];
-            clock_counter_b <= 9'b000000000;
+            clock_counter_b <= 7'b0000000;
         end
         if (clock_div_factor_c < clock_counter_c) begin
             div_clock[2] <= ~div_clock[2];
-            clock_counter_c <= 9'b000000000;
+            clock_counter_c <= 7'b0000000;
         end
         if (clock_div_factor_d < clock_counter_d) begin
             div_clock[3] <= ~div_clock[3];
-            clock_counter_d <= 9'b000000000;
+            clock_counter_d <= 7'b0000000;
         end
     end
     
